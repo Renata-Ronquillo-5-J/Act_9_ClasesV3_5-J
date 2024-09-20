05eb98cce33dd7114698fcc6ca132e1515717272
Renata-Ronquillo <a.22308051281307@cbtis128.edu.mx>
2024-09-20 09:35:55 -0600

    Add files via upload

---
+++ OperadoresA.py
@@ -0,0 +1,64 @@
+print(" \n Act 9 Clases V3")
+print(" Renata ronquilo Lopez: 22308051281307 ♥ ")
+# Zona de class
+
+class Operadores1307:
+    def suma1307(self, R, L):
+        s1307=R + L
+        print(f" La suma de {R} + {L} = {s1307} ♥ \n")
+        
+    def resta1307(self, R, L):
+        r1307= R+L
+        print(f" La resta de {R} - {L} = {r1307} ♥ \n")
+        
+    def Multi1307(self, R, L):
+        M1307= R*L
+        print(f" La Multiplicación de {R} * {L} = {M1307} ♥ \n")
+        
+    def Divi1307(self, R, L):
+        d1307=R/L
+        print(f" La Division de {R} / {L} = {d1307} ♥ \n")
+        
+    def Modu1307(self, R, L):
+        MO1307= R%L
+        print(f" El modulo de {R} % {L} = {MO1307} ♥ \n")
+        
+    def Ex1307(self, R, L):
+        e1307= R**L
+        print(f" El exponente de {R} ** {L} = {e1307} ♥ \n")
+        
+    def Co1307(self, R, L):
+        C1307= R//L
+        print(f" El Cosiente de {R} // {L} = {C1307} ♥ \n")
+# Zona de creacion del objeto 
+
+OperoRonquillo=Operadores1307()
+
+
+# Zona de uso de objeto
+print( " Funcion Suma: ")
+OperoRonquillo.suma1307(5,2)
+
+print(" Funcion Resta: ")
+OperoRonquillo.resta1307( 5,5)
+
+print( " Funcio Multiplicación: ")
+OperoRonquillo.Multi1307(1,3)
+
+print( "Funciones Division:  ")
+OperoRonquillo.Divi1307( 4,2)
+
+print( " Funciones de Modulo: ")
+OperoRonquillo.Modu1307( 10, 2) 
+
+print( " Funciones de Exponente: ")
+OperoRonquillo.Ex1307( 3, 2) 
+
+print( " Funciones de Cosiente: ")
+OperoRonquillo.Co1307( 20, 2) 
+
+
+
+
+
+
